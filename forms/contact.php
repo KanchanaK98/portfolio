cf4db032dfe4297c939755a44b9febd39660074b
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2023-06-10 12:36:07 +0530

    changed contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
   $name = $_POST["name"];
   $email = $_POST["email"];
   $message = $_POST["message"];
@@ -19,5 +19,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     echo "Oops! Something went wrong. Please try again later.";
   }
-}
+
 ?>
